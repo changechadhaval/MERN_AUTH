3e99e77749c8c3c27ac31eb39fce562edbaff0eb
changechadhaval <changechadhaval100@gmail.com>
2025-08-14 11:39:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend
+    origin: "mern-auth-frontend-xeee.onrender.com", // your frontend
     credentials: true, // allow cookies
   })
 );
